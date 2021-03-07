3e31e592d88984bb0a0c42cd2c82a647f5564ea2
tvattima <github@vattima.com>
2021-03-07 16:31:04 -0500

    * Upgrades to JDK 11, Spring 2.4.3, Junit 5, other latest libraries

--- src/test/java/ShipStationTest.java
+++ src/test/java/ShipStationTest.java
@@ -1,10 +1,11 @@
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.Base64;
 
-public class ShipStationTest {
+class ShipStationTest {
     @Test
-    public void shipstation_usernamePassword() {
+    void shipstation_usernamePassword() {
         System.out.println(Base64.getEncoder().encodeToString("9ea9051c16aa443baf1700f48eda34b6:2a28229f1864405b97afb3ace18ddfcf".getBytes()));
     }
 }
